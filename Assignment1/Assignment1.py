4baf1f166e40feeeb248e32627251d8503d604dd
Divya Bhegade <divya.bhegade51@gmail.com>
2026-01-22 16:31:24 +0530

    Delete Assignment1.py

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -1,13 +1,1 @@
-#Task 1
-num1=int(input("Enter the first number:"))
-num2=int(input("Enter the second number:"))
-print("Addition:",num1+num2)
-print("Subtraction:",num1-num2)
-print("Multiplication:",num1*num2)
-print("Division:",num1/num2)
 
-#Task 2
-a=input("Enter your first name:")
-b=input("Enter your last name:")
-c=a+b
-print("Hello,",c,"!","Welcome to the python program.")
